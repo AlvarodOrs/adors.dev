b60030a0b75e61ae0e220afcadbdb008fed05666
Álvaro d'Ors Nestares <75393390+AlvarodOrs@users.noreply.github.com>
2026-05-30 23:56:57 +0200

    Project categories

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  // Required for Cloudflare Pages
+const nextConfig = {
+  output: 'export',
 }
 
 export default nextConfig
